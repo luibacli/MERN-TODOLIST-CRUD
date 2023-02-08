186990e2f49a80f60a9589cfcfb4a4ba6f8424b1
luibacli <luibacli@gmail.com>
2023-02-08 21:44:53 +0800

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-// const connectDB = require("./mongodb");
+const connectDB = require("./mongodb");
 const todoRoute = require("./routes/todoRoute");
 const app = express();
 const PORT = 5000;
 app.use(express.json());
 app.use(cors());
-
-// connectDB();
-
+require("dotenv").config({ path: "./config.env" });
+connectDB();
 app.use("/api", todoRoute);
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
