5024697bfe9b38ca39e607a3ac749d689587ad62
luibacli <luibacli@gmail.com>
2023-02-18 00:19:35 +0800

    finalizing .env and package.json

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
+require("dotenv").config();
 const cors = require("cors");
 const connectDB = require("./mongodb");
 const todoRoute = require("./routes/todoRoute");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
-require("dotenv").config({ path: "./config.env" });
+
 connectDB();
 app.use("/api", todoRoute);
 
